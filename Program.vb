abbd59261e2bf98c75a3f36f92af769cf4957c1b
David Tyler <david.tyler@metaswitch.com>
2021-05-12 13:43:41 +0100

    Stop logging so much crap during each LDA action

--- Program.vb
+++ Program.vb
@@ -1,8 +1,9 @@
 Imports System
-Imports Microsoft.AspNetCore.Hosting
 Imports Microsoft.AspNetCore
-Imports Microsoft.Extensions.DependencyInjection
 Imports Microsoft.AspNetCore.Builder
+Imports Microsoft.AspNetCore.Hosting
+Imports Microsoft.Extensions.DependencyInjection
+Imports Microsoft.Extensions.Logging
 
 Module Program
     Sub Main(args As String())
@@ -10,6 +11,9 @@ Module Program
     End Sub
 
     Function BuildWebHost(args As String()) As IWebHost
-        Return WebHost.CreateDefaultBuilder(args).UseStartup(Of Startup)().Build()
+        Return WebHost.CreateDefaultBuilder(args).UseStartup(Of Startup)().ConfigureLogging(Function (log as ILoggingBuilder)
+                log.AddFilter("Microsoft", LogLevel.Warning).AddFilter("System", LogLevel.Warning).AddConsole()
+                Return log
+            End Function).Build()
     End Function
 End Module
